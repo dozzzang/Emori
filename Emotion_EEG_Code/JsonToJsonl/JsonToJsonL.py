2287f3cabad37f996fbb0c5f3db3eed97475a921
KimTaeHyeong <xogud3624@gmail.com>
2025-10-06 15:52:34 +0900

    기존 필요없는 상수 제거 및 데이터 경로 지정

--- Emotion_EEG_Code/JsonToJsonl/JsonToJsonL.py
+++ Emotion_EEG_Code/JsonToJsonl/JsonToJsonL.py
@@ -1,57 +1,19 @@
 import json
 import pandas as pd
+import os, sys
 
-# ===== 1) 용어/사전 =====
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
-EMOTION_KO = {
-    "Happy": "기쁜",
-    "Fear": "두려운",
-    "Sad": "슬픈",
-    "Surprise": "놀란",
-    "Angry": "화나는",
-    "Disgust": "싫은",
-}
+# 이 경로는 실행 스크립트의 위치에 따라 조정이 필요할 수 있습니다.
+project_root = os.path.join(current_script_dir, "..")
 
-HAPPY_QUALITY = {"Low": "만족", "Half": "편안한", "High": "기쁜", "Full": "신나는"}
-FEAR_QUALITY = {
-    "Low": "불안한",
-    "Half": "겁나는",
-    "High": "두려운",
-    "Full": "공포스러운",
-}
-SAD_QUALITY = {"Low": "속상한", "Half": "걱정되는", "High": "슬픈", "Full": "절망적인"}
-SURPRISE_QUALITY = {
-    "Low": "어이가 없는",
-    "Half": "긴장되는",
-    "High": "깜짝 놀란",
-    "Full": "놀라 충격적인",
-}
-ANGRY_QUALITY = {
-    "Low": "섭섭한",
-    "Half": "짜증나는",
-    "High": "화나는",
-    "Full": "분노로 가득찬",
-}
-DISGUST_QUALITY = {
-    "Low": "지겨운",
-    "Half": "싫증나는",
-    "High": "너무 싫은",
-    "Full": "역겨운",
-}
+if project_root not in sys.path:
+    sys.path.append(project_root)
 
-BODY_FEEL = {
-    "Minimal": "발목까지 올라오는",
-    "Low": "무릎까지 올라오는",
-    "Half": "허리까지 올라오는",
-    "High": "가슴까지 차오르는",
-    "Full": "온몸을 가득 채운",
-}
+import constants
 
-LABEL = {
-    "focus": "집중",
-    "interest": "흥미",
-    "engage": "관여도",
-    "excite": "각성",
-    "relax": "이완",
-    "stress": "스트레스",
-}
+SRC = constants.OUTPUT_JSON_FILE
+LABEL_CSV = constants.ASSISTANT_LABELS
+OUT = constants.TRAIN_JSONL_FILE
+
+
