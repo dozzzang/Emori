af3231c0bc34fd7f8f2ce0f8d6cffa146a0663b8
KimTaeHyeong <xogud3624@gmail.com>
2025-10-08 13:15:34 +0900

    Data에 실제 수행할 json 예제 추가 및 constants에 등록

--- Emotion_EEG_Code/constants.py
+++ Emotion_EEG_Code/constants.py
@@ -8,3 +8,6 @@ BASE_INPUT_FILE = os.path.join(DATA_DIR, "RECORD_20250515__1.txt")
 LLAMA3_ADAPTER = os.path.join(DATA_DIR, "Llama3_Result")
 ASSISTANT_LABELS = os.path.join(DATA_DIR, "Manual_Assistant_Labels.csv")
 TRAIN_JSONL_FILE = os.path.join(DATA_DIR, "Train_Data.jsonl")
+
+# 실제로 프로그램을 사용할 때 필요한 데이터
+MAIN_JSON_FILE = os.path.join(DATA_DIR, "Report_Data.json")
